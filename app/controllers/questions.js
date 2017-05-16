7bd4de27b757e554fb1c4d6d9d442958d5e0d31a
lisamsmith100 <lisamarie.smith@libertymutual.com>
2017-05-16 16:29:56 -0400

    Add code for routes, controller, model for the questions collection.
    We are amidst troubleshooting the create curl requests and
    receiving undefined.  We decided to push this code so we can individually
    troubleshoot.

---
+++ app/controllers/questions.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const controller = require('lib/wiring/controller')
+const models = require('app/models')
+const Question = models.question
+
+const authenticate = require('./concerns/authenticate')
+const setUser = require('./concerns/set-current-user')
+const setModel = require('./concerns/set-mongoose-model')
+
+const index = (req, res, next) => {
+  Question.find()
+    .then(questions => res.json({
+      questions: questions.map((e) =>
+        e.toJSON({ virtuals: true, user: req.user })),
+    }))
+    .catch(next)
+}
+
+const show = (req, res) => {
+  res.json({
+    question: req.question.toJSON({ virtuals: true, user: req.user }),
+  })
+}
+
+const create = (req, res, next) => {
+  // console.log('this is req.body ', req.body)
+  console.log("req.body.question: ", req.body.question)
+  // console.log('this is req.question._survey ', req.question._survey)
+  console.log('req.body.user is ', req.body.user)
+  const question = Object.assign(req.body.question, {
+    _owner: req.body.user._id,
+    _survey: req.body.question._survey
+  })
+  Question.create(question)
+    .then(question =>
+      res.status(201)
+        .json({
+          question: question.toJSON({ virtuals: true, user: req.user }),
+        }))
+    .catch(next)
+}
+
+const update = (req, res, next) => {
+  delete req.body._owner  // disallow owner reassignment.
+  req.question.update(req.body.question)
+    .then(() => res.sendStatus(204))
+    .catch(next)
+}
+
+const destroy = (req, res, next) => {
+  req.question.remove()
+    .then(() => res.sendStatus(204))
+    .catch(next)
+}
+
+module.exports = controller({
+  index,
+  show,
+  create,
+  update,
+  destroy,
+}, { before: [
+  { method: setUser, only: ['index', 'show'] },
+  { method: authenticate, except: ['index', 'show'] },
+  { method: setModel(Question), only: ['show'] },
+  { method: setModel(Question, { forUser: true }), only: ['update', 'destroy'] },
+], })
--- scripts/sign-in.sh
+++ scripts/sign-in.sh
@@ -2,8 +2,8 @@
 
 API="http://localhost:4741"
 URL_PATH="/sign-in"
-EMAIL="lucky@yahoo.com"
-PASSWORD="test"
+EEMAIL=lmslms@lisa.com
+PASSWORD=l
 curl "${API}${URL_PATH}" \
   --include \
   --request POST \
