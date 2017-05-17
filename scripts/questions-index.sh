cd20213761753f9a8d8e33204aeab74924392f4b
antleo1995 <antleo1995@gmail.com>
2017-05-17 14:08:53 -0400

    Makes curls request for questions
    Makes a few curl requests to get a new working route for questions
    to pull them back by ID.

--- scripts/questions-index.sh
+++ scripts/questions-index.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-
+ID="591c84247cd6579770d5f234"
 API="http://localhost:4741"
-URL_PATH="/questions"
+URL_PATH="/questionsbysurvey"
 TOKEN="kJ4oKiWr7ra0yFJowFSg2TFidyU1ilziJXDcNoc0ZeA=--1y3UEL38wGTnUhRPI3JP9hMO6aWKyMXrhao1Z5xY/Kc="
-QUESTION="MY Fourth Question to my survey"
-QUESTIONNUMBER="4"
-_SURVEY="591c84247cd6579770d5f234"
 curl "${API}${URL_PATH}" \
   --include \
-  --request POST \
+  --request GET \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
-    "question": {
-      "question": "'"${QUESTION}"'",
-      "questionNumber": "'"${QUESTIONNUMBER}"'",
-      "_survey": "'"${_SURVEY}"'"
+    "survey": {
+      "id": "'"${ID}"'"
     }
   }'
 
--- scripts/survey-index.sh
+++ scripts/survey-index.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-
+ID="591c84247cd6579770d5f234"
 API="http://localhost:4741"
 URL_PATH="/surveys"
-TOKEN="OmPynh2gBneOPrpgBvBgHey/NToPkvJkCYs8IWfgc70=--atKmqmQLhsVj/uiVbJSog2SNb6v1D19Qu4/zTlx/W7s="
+TOKEN="kJ4oKiWr7ra0yFJowFSg2TFidyU1ilziJXDcNoc0ZeA=--1y3UEL38wGTnUhRPI3JP9hMO6aWKyMXrhao1Z5xY/Kc="
 curl "${API}${URL_PATH}" \
   --include \
   --request GET \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=${TOKEN}"
+  --header "Authorization: Token token=${TOKEN}"\
+  --data '{
+    "survey": {
+      id:"'"${ID}"'"
+    }
+  }'
 
 echo
